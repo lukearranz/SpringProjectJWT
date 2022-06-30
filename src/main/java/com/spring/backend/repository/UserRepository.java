dbfab5dd0e8b62186e99ed978c614bd1963972c5
lukearranz <lucas.arranz@hotmail.com>
2022-06-30 09:12:04 -0300

    first commit, to save this project in GH

---
+++ src/main/java/com/spring/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.spring.backend.repository;
+
+import com.spring.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/spring/backend/service/UserService.java
@@ -0,0 +1,14 @@
+package com.spring.backend.service;
+
+import com.spring.backend.domain.Role;
+import com.spring.backend.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
+}
